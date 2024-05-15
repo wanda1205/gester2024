9333263641369004f110cec25a393916bb542160
wanda1205 <aa0925653552@gmail.com>
2024-05-15 17:07:14 +0800

    v2

--- app/src/main/java/pu/edu/tw/wanda/gester/MainActivity.kt
+++ app/src/main/java/pu/edu/tw/wanda/gester/MainActivity.kt
@@ -3,12 +3,25 @@ package pu.edu.tw.wanda.gester
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import pu.edu.tw.wanda.gester.ui.theme.GesterTheme
 
@@ -19,7 +32,9 @@ class MainActivity : ComponentActivity() {
             GesterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    //PointerEvents()
+                    Tap()
                 }
             }
         }
@@ -27,17 +42,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GesterTheme {
-        Greeting("Android")
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("") }
+    Column {
+        Text(msg)
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                }
+        )
     }
 }
+@Composable
+fun Tap() {
+    var msg by remember { mutableStateOf("TAP相關手勢實例") }
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset.Zero) }
+    var PU = arrayListOf(R.drawable.pu0, R.drawable.pu1,
+        R.drawable.pu2, R.drawable.pu3,
+        R.drawable.pu4, R.drawable.pu5)
+    var Number by remember { mutableStateOf(0) }
+
+
+    Column {
+        Text(text = msg)
+
+        Image(
+            painter = painterResource(id = PU[Number]),
+            contentDescription = "靜宜之美",
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGesturesAfterLongPress(
+                        onDrag = { change, dragAmount -> offset2+=dragAmount},
+                        onDragStart = {
+                            offset1 = it
+                            offset2 = it },
+                        onDragEnd = {if (offset2.x >= offset1.x){
+                            msg = "長按後向右拖曳"
+                            Number ++
+                            if (Number>5){Number=0}
+                        }
+                        else{
+                            msg = "長按後向左拖曳"
+                            Number --
+                            if (Number<0){Number=5}
+                        }
+                        }
+                    )
+                }
+
+        )
+    }
+}
+
